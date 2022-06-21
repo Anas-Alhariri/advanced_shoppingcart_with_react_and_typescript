52081b850df24f5c54a5dfbf8d5ff345c844f648
Anas Alhariri <anasalhariri@Anass-MacBook-Pro.local>
2022-06-21 17:18:27 -0400

    Created the nav, router, routes, button for the cart with the badget for updating the numer of items in the cart, and added links for the pages in the navbar and created the pages functional components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { Button, Container, Nav, Navbar as NavbarBS } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+import cartImage from "../imgs/shopping-cart-10925.svg";
+
+const Navbar = () => {
+  return (
+    <>
+      <NavbarBS sticky="top" className="bg-white shadow-sm mb-3">
+        <Container>
+          <Nav className="me-auto">
+            <Nav.Link to="/" as={NavLink}>
+              Home
+            </Nav.Link>
+            <Nav.Link to="/store" as={NavLink}>
+              Store
+            </Nav.Link>
+            <Nav.Link to="/about" as={NavLink}>
+              About
+            </Nav.Link>
+          </Nav>
+          <div
+            style={{
+              position: "relative",
+            }}
+          >
+            <img
+              src={cartImage}
+              alt=""
+              style={{
+                width: "3rem",
+                height: "3rem",
+                cursor: "pointer",
+                border: ".8px solid black",
+                borderRadius: "50%",
+                padding: "10px",
+              }}
+            />
+            <div
+              style={{
+                position: "absolute",
+                bottom: "0",
+                right: "0",
+                width: "1.5rem",
+                height: "1.5rem",
+                transform: "translate(25%,25%)",
+                backgroundColor: "lightgreen",
+                borderRadius: "50%",
+                color: "red",
+                display: "flex",
+                placeContent: "center",
+                placeItems: "center",
+                fontSize: ".85rem",
+              }}
+            >
+              3
+            </div>
+          </div>
+        </Container>
+      </NavbarBS>
+    </>
+  );
+};
+
+export default Navbar;
